3ad9e6793add80ac6967dfbafa13b02f3935bf62
Jeff Bailey <jbailey2010@gmail.com>
2017-05-15 11:21:06 -0700

    Added async task to refresh Cogneato credentials

--- FFRv2/app/src/main/java/com/devingotaswitch/ffrv2/cognito/CognitoClientManager.java
+++ FFRv2/app/src/main/java/com/devingotaswitch/ffrv2/cognito/CognitoClientManager.java
@@ -2,6 +2,8 @@ package com.devingotaswitch.ffrv2.cognito;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.auth.CognitoCredentialsProvider;
@@ -11,6 +13,7 @@ public class CognitoClientManager {
 
     private static final String IDENTITY_POOL_ID = "us-west-2:2dbf85d8-fe1f-4e97-9c93-a4731883aed5";
     private static final Regions REGION = Regions.US_WEST_2;
+    private static final String COGNITO_TAG = "CognitoClientManager";
 
     private static CognitoCredentialsProvider credentialsProvider;
 
@@ -23,5 +26,21 @@ public class CognitoClientManager {
             );
         }
         return credentialsProvider;
+    }
+
+    public void refreshCredentialsAsync(Context context) {
+        new CredentialsRefresh().execute(getCredentialsProvider(context));
+    }
+    private class CredentialsRefresh extends AsyncTask<Object, Void, Void> {
+        @Override
+        protected Void doInBackground(Object... params) {
+            CognitoCredentialsProvider provider = (CognitoCredentialsProvider) params[0];
+            try {
+                provider.refresh();
+            } catch (Exception e) {
+                Log.e(COGNITO_TAG, "Failed to refresh credentials", e);
+            }
+            return null;
+        }
     }
 }
