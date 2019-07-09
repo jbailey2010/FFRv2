9522ffb516e06d369b241ff0f9f7bdf2ff5b67cc
Jeff Bailey <jbailey2010@gmail.com>
2019-07-08 23:29:10 -0700

    Fixed OL

--- FFRv2/app/src/main/java/com/devingotaswitch/rankings/sources/ParsePFO.java
+++ FFRv2/app/src/main/java/com/devingotaswitch/rankings/sources/ParsePFO.java
@@ -1,5 +1,7 @@
 package com.devingotaswitch.rankings.sources;
 
+import android.util.Log;
+
 import com.devingotaswitch.rankings.domain.Rankings;
 import com.devingotaswitch.utils.Constants;
 import com.devingotaswitch.utils.GeneralUtils;
@@ -19,7 +21,6 @@ public class ParsePFO {
     public static void parsePFOLineData(Rankings rankings) throws IOException {
         List<String> td = JsoupUtils.parseURLWithUA(
                 "http://www.footballoutsiders.com/stats/ol", "table.stats td");
-        Map<String, String> data = new HashMap<>();
         int start = 0;
         for (int i = 0; i < td.size(); i++) {
             if (GeneralUtils.isInteger(td.get(i))) {
@@ -27,24 +28,20 @@ public class ParsePFO {
                 break;
             }
         }
-        for (int i = start; i < td.size(); i += 16) {
+        for (int i = start; i < td.size(); i += 15) {
             if (td.get(i).equals("RUN BLOCKING")) {
                 i += 2;
                 continue;
             } else if (td.get(i + 1).equals("Team") || "".equals(td.get(i+1))) {
-                i += 16;
+                i += 15;
                 continue;
             } else if (td.get(i + 1).equals("NFL")) {
                 break;
             }
 
-            String team2 = ParsingUtils.normalizeTeams(td.get(i + 12));
-            String sack = td.get(i + 15);
-            data.put(team2 + "/pass", sack + " adjusted team sack rate ("
-                    + td.get(i + 13) + ")\n");
-            data.put(team2 + "/passranks",
-                    "Pass Block Ranking: " + td.get(i + 13));
-            String team1 = ParsingUtils.normalizeTeams(td.get(i + 1));
+            String team = ParsingUtils.normalizeTeams(td.get(i+1));
+            String sackRate = td.get(i + 14);
+            String pbRank = td.get(i + 12);
             String adjYPC = td.get(i + 2);
             String adjYPCRank = td.get(i);
             String power = td.get(i + 4);
@@ -55,63 +52,47 @@ public class ParsePFO {
             String secLevelRank = td.get(i + 9);
             String openField = td.get(i + 10);
             String openFieldRank = td.get(i + 11);
-            String runData = adjYPC + " adjusted team yards per carry (" + adjYPCRank + ")\n" +
-                    power + " success rate with < 3 yards per go (" + powerRank + ")\n" +
-                    stuff + " rate of being stuffed at the line (" + stuffRank + ")\n" +
-                    secLevel + " YPC earned between 5 and 10 yards past LOS (" + secLevelRank + ")\n" +
-                    openField + " YPC earned 10+ yards past LOS (" + openFieldRank + ")";
-            data.put(team1 + "/run", runData);
-            data.put(team1 + "/runranks", "Run Block Ranking: " + adjYPCRank);
-        }
-        List<String> teams = new ArrayList<>();
-        Set<String> keys = data.keySet();
-        for (String key : keys) {
-            String team = key.split("/")[0];
-            if (!teams.contains(team)) {
-                teams.add(team);
-            }
-        }
-        PriorityQueue<String> math = new PriorityQueue<>(100,
-                new Comparator<String>() {
-                    @Override
-                    public int compare(String a, String b) {
-                        String runA = a.split("~~~~")[1].split(": ")[1];
-                        String runB = b.split("~~~~")[1].split(": ")[1];
-                        String passA = a.split("~~~~")[0].split(": ")[1];
-                        String passB = b.split("~~~~")[0].split(": ")[1];
-                        double overallA = (double) (Integer.parseInt(runA) + Integer
-                                .parseInt(passA)) / 2.0;
-                        double overallB = (double) (Integer.parseInt(runB) + Integer
-                                .parseInt(passB)) / 2.0;
-                        return Double.compare(overallA, overallB);
-                    }
-                });
-        for (String team : teams) {
-            String input = data.get(team + "/passranks") + "~~~~"
-                    + data.get(team + "/runranks") + "~~~~" + team;
-            if (input.split("~~~~")[1].split(": ").length > 1) {
-                math.add(input);
-            }
-        }
-        if (math.size() > 0) {
-            for (int i = 1; i < 33; i++) {
-                String full = math.poll();
-                String team = full.split("~~~~")[2];
-                data.put(team + "/overallranks", String.valueOf(i));
-            }
-        }
-        for (String team : teams) {
-            String val = data.get(team + "/pass") +
-                    data.get(team + "/run") +
-                    Constants.LINE_BREAK +
-                    Constants.LINE_BREAK +
-                    data.get(team + "/passranks") +
-                    Constants.LINE_BREAK +
-                    data.get(team + "/runranks") +
-                    Constants.LINE_BREAK +
-                    "Overall Ranking: " +
-                    data.get(team + "/overallranks");
-            rankings.getTeam(team).setoLineRanks(val);
+
+            String olData = new StringBuilder(sackRate)
+                    .append(" adjusted team sack rate.")
+                    .append(Constants.LINE_BREAK)
+                    .append(adjYPC)
+                    .append(" adjusted team yards per carry (")
+                    .append(adjYPCRank)
+                    .append(")")
+                    .append(Constants.LINE_BREAK)
+                    .append("Pass Block Ranking: ")
+                    .append(pbRank)
+                    .append(Constants.LINE_BREAK)
+                    .append(power)
+                    .append(" success rate with < 3 yards to go (")
+                    .append(powerRank)
+                    .append(")")
+                    .append(Constants.LINE_BREAK)
+                    .append(stuff)
+                    .append(" rate of being stuffed at the line (")
+                    .append(stuffRank)
+                    .append(")")
+                    .append(Constants.LINE_BREAK)
+                    .append(secLevel)
+                    .append(" YPC earned 5 to 10 yards past LOS (")
+                    .append(secLevelRank)
+                    .append(")")
+                    .append(Constants.LINE_BREAK)
+                    .append(openField)
+                    .append(" YPC earned 10+ yards past LOS (")
+                    .append(openFieldRank)
+                    .append(")")
+                    .append(Constants.LINE_BREAK)
+                    .append(Constants.LINE_BREAK)
+                    .append("Pass Block Ranking: ")
+                    .append(pbRank)
+                    .append(Constants.LINE_BREAK)
+                    .append("Run Block Ranking: ")
+                    .append(adjYPCRank)
+                    .toString();
+
+            rankings.getTeam(team).setoLineRanks(olData);
         }
     }
 }
