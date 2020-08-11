1017dd7ae7afa9293b6acbfda7b0ec5f6ec8bc6c
Jeff Bailey <jbailey2010@gmail.com>
2020-08-10 21:10:14 -0700

    Turned one pojo into kotlin as a poc

---
+++ FFRv2/app/src/main/java/com/devingotaswitch/rankings/domain/UserSettings.kt
@@ -0,0 +1,15 @@
+package com.devingotaswitch.rankings.domain
+
+class UserSettings {
+    var isHideDraftedSearch = false
+    var isHideDraftedSort = false
+    var isHideDraftedComparator = false
+    var isHideRanklessSearch = false
+    var isHideRanklessSort = false
+    var isHideRanklessComparator = false
+    var isShowNoteRank = true
+    var isShowNoteSort = true
+    var isRefreshOnOverscroll = false
+    var isSortWatchListByTime = false
+
+}
