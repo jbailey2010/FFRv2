6623d72529caf51724d3b296c87e3e6baf53dbc6
Jeff Bailey <jbailey2010@gmail.com>
2020-08-16 19:45:00 -0700

    Draft utils to kotlin (it's weirdly difficult implementing kotlin interfaces inline as vars)

---
+++ FFRv2/app/src/main/java/com/devingotaswitch/utils/DraftUtils.kt
@@ -0,0 +1,69 @@
+package com.devingotaswitch.utils
+
+import android.app.Activity
+import android.content.DialogInterface
+import android.text.InputType
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.andrognito.flashbar.Flashbar
+import com.andrognito.flashbar.Flashbar.OnActionTapListener
+import com.devingotaswitch.ffrv2.R
+import com.devingotaswitch.rankings.domain.Player
+import com.devingotaswitch.rankings.domain.Rankings
+import com.devingotaswitch.rankings.extras.RecyclerViewAdapter
+import com.devingotaswitch.utils.GeneralUtils.isInteger
+
+object DraftUtils {
+    @JvmStatic
+    fun getUndraftListener(activity: Activity?, rankings: Rankings, player: Player?,
+                           view: View?, adapter: RecyclerViewAdapter, data: MutableList<Map<String?, String?>?>,
+                           datum: MutableMap<String?, String?>, position: Int, updateList: Boolean): OnActionTapListener {
+        return object : OnActionTapListener {
+            override fun onActionTapped(bar: Flashbar) {
+                rankings.draft.undraft(rankings, player, activity, view)
+                if (updateList) {
+                    data.add(position, datum)
+                } else {
+                    datum[Constants.PLAYER_ADDITIONAL_INFO] = ""
+                }
+                adapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    @JvmStatic
+    fun getAuctionCostDialog(activity: Activity?, player: Player, callback: AuctionCostInterface): AlertDialog {
+        val li = LayoutInflater.from(activity)
+        val noteView = li.inflate(R.layout.user_input_popup, null)
+        val alertDialogBuilder = AlertDialog.Builder(
+                activity!!)
+        alertDialogBuilder.setView(noteView)
+        val userInput = noteView
+                .findViewById<EditText>(R.id.user_input_popup_input)
+        userInput.inputType = InputType.TYPE_CLASS_NUMBER
+        userInput.hint = "Auction cost"
+        val title = noteView.findViewById<TextView>(R.id.user_input_popup_title)
+        title.text = "How much did " + player.name + " cost?"
+        alertDialogBuilder
+                .setPositiveButton("Save") { dialogInterface: DialogInterface?, i: Int ->
+                    val input = userInput.text.toString()
+                    if (input.isEmpty() || !isInteger(input)) {
+                        callback.onInvalidInput()
+                    } else {
+                        callback.onValidInput(input.toInt())
+                    }
+                }
+                .setNegativeButton("Cancel") { dialogInterface: DialogInterface?, i: Int -> callback.onCancel() }
+        alertDialogBuilder.setCancelable(false)
+        return alertDialogBuilder.create()
+    }
+
+    interface AuctionCostInterface {
+        fun onValidInput(cost: Int?)
+        fun onInvalidInput()
+        fun onCancel()
+    }
+}
